5eaf01b3e585828fedc60c4f949f8aee0f1d16ea
ericreed <reeder@bu.edu>
2021-03-04 15:53:06 -0500

    Added qsub example

--- scripts/00_install.R
+++ scripts/00_install.R
@@ -1,7 +1,7 @@
 # Run on the SCC with R v3.6.0
 
 # K2Taxonomer package
-devtools::install_github("sac4vf/ConAn")
+devtools::install_github("PATH2RECENTVERSION")
 
 # Functions for running simulations
 install.packages(c("reticulate", "Matrix", "BDgraph", "corpcor", "MASS", "igraph"), repos='http://cran.us.r-project.org')
